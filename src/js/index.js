ebfefb6440c6c56348a7ac5a0649c151e29e4995
xyrlan; pedro <68228285+xyrlan@users.noreply.github.com>
2023-03-15 16:00:53 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const botoesCarrosel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+
+botoesCarrosel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        const botaoSelecionado = document.querySelector('.selecionado');
+        botaoSelecionado.classList.remove('selecionado');
+
+        botao.classList.add('selecionado');
+
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remove('ativa'); 
+
+        imagens[indice].classList.add('ativa');
+
+        
+
+    })
+})
+
+// $('.carousel').carousel()
